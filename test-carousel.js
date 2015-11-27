ffb553ae39d81398dc792252df206bba84e36058
yingran <yingran@google.com>
2015-11-27 14:51:51 +1100

    working carousel with 3 images horizontal, no animation yet

--- test-carousel.js
+++ test-carousel.js
@@ -9,38 +9,65 @@ Polymer({
       reflectToAttribute: true
     },
     title: {
-      type: String,
-      value: "1",
+      type: Number,
+      value: 1,
       reflectToAttribute: true
     },
     mysrc: {
       type: String,
       value: "/carousel/images/1.jpg"
     },
-    numImages: {
+    myprev: {
       type: String,
-      value: "3"
+      value: "/carousel/images/8.jpg"
+    },
+    mynext: {
+      type: String,
+      value: "/carousel/images/2.jpg"
+    },
+    numImages: {
+      type: Number,
+      value: 8
     },
   },
 
   nextImage: function(){
     this.title++;
-    if(this.title > this.numImages){
-      this.title="1";
-    }
-
     var myfolder = "/carousel/images/";
+
+    if(this.title == (this.numImages + 1)){
+      this.title=1;
+      this.myprev=myfolder.concat((this.numImages), ".jpg");
+      this.mynext=myfolder.concat((this.title + 1), ".jpg");
+    } else if (this.title == this.numImages ){
+      this.mynext=myfolder.concat("1", ".jpg");
+      this.myprev=myfolder.concat((this.title - 1), ".jpg");      
+    } else {
+      this.myprev=myfolder.concat((this.title - 1), ".jpg");
+      this.mynext=myfolder.concat((this.title + 1), ".jpg");
+    }
+    
     this.mysrc= myfolder.concat(this.title, ".jpg");
+
   },
 
   prevImage: function(){
     this.title--;
-
-    if(this.title < 1) {
-      this.title= this.numImages;
-    }
-
     var myfolder = "/carousel/images/";
+
+    if(this.title == 1) {
+      this.mynext=myfolder.concat((this.title + 1), ".jpg");
+      this.myprev=myfolder.concat(this.numImages, ".jpg"); 
+    } else if (this.title == 0) {
+      this.title= this.numImages;
+      this.mynext= myfolder.concat("1", ".jpg");
+      this.myprev= myfolder.concat((this.title -1 ), ".jpg");    
+    } else {
+      this.myprev=myfolder.concat((this.title -1), ".jpg");
+      this.mynext=myfolder.concat((this.title + 1 ), ".jpg");      
+    }
     this.mysrc= myfolder.concat(this.title, ".jpg");
+
   }
+
 });
